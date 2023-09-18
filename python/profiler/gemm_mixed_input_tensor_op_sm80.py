9514f99e7924254366c87ff85443a23523de181f
Manish Gupta <manigupta@google.com>
2023-09-18 22:39:52 +0000

    save

--- python/profiler/gemm_mixed_input_tensor_op_sm80.py
+++ python/profiler/gemm_mixed_input_tensor_op_sm80.py
@@ -7,7 +7,7 @@ import torch
 
 # Matmul problem shape to fill 108 SMs on A100 and test instruction mix and scheduling 
 # (decoupling auto-tuning)
-problem_shape = ProblemShape(3456, 4096, 2048)
+problem_shape = ProblemShape(3456, 4096, 8192)
 #problem_shape = ProblemShape(128, 256, 512)
 
 
@@ -23,8 +23,8 @@ matmul_description = []
 # RowMajor * ColumnMajor / TN GEMM with F16 operand A and I8 operand B
 matmul_description.append(MatmulDescription(dtype_a=torch.float16, layout_a=MatrixLayout.RowMajor,
                                             dtype_b=torch.int8, layout_b=MatrixLayout.ColumnMajor,
-                                            dtype_c=torch.float32, layout_c=MatrixLayout.RowMajor,
-                                            dtype_d=torch.float32, 
+                                            dtype_c=torch.float16, layout_c=MatrixLayout.RowMajor,
+                                            dtype_d=torch.float16, 
                                             dtype_accumulator=torch.float32))
 
 
@@ -34,8 +34,8 @@ matmul_description.append(MatmulDescription(dtype_a=torch.float16, layout_a=Matr
 # RowMajor * ColumnMajor / TN GEMM with BF16 operand A and I8 operand B
 matmul_description.append(MatmulDescription(dtype_a=torch.bfloat16, layout_a=MatrixLayout.RowMajor,
                                             dtype_b=torch.int8, layout_b=MatrixLayout.ColumnMajor,
-                                            dtype_c=torch.float32, layout_c=MatrixLayout.RowMajor,
-                                            dtype_d=torch.float32, 
+                                            dtype_c=torch.bfloat16, layout_c=MatrixLayout.RowMajor,
+                                            dtype_d=torch.bfloat16, 
                                             dtype_accumulator=torch.float32))
 
 for tile_config in tile_configurations:
--- python/profiler/gemm_tensor_op_sm80.py
+++ python/profiler/gemm_tensor_op_sm80.py
@@ -11,7 +11,7 @@ problem_shape = ProblemShape(3456, 4096, 8192)
 
 # Create a list of tile configurations 
 tile_configurations = []
-tile_configurations.append(TileConfiguration(kCtaTileM=128, kCtaTileN=128, kCtaTileK=64, kNumStages=4))
+tile_configurations.append(TileConfiguration(kCtaTileM=128, kCtaTileN=128, kCtaTileK=64, kNumStages=3))
 
 # Create a list of matmul descriptions for F16 Tensor Cores
 matmul_description = []
@@ -44,13 +44,13 @@ matmul_description.append(MatmulDescription(dtype_a=torch.float16, layout_a=Matr
                                             dtype_accumulator=torch.float16))
 
 ##################################################################################################
-###                          F32 <= BF16 * BF16 + F32 
+###                          BF16 <= BF16 * BF16 + F32 
 ##################################################################################################
 # RowMajor * ColumnMajor / TN GEMM with BF16 operand A and I8 operand B
 matmul_description.append(MatmulDescription(dtype_a=torch.bfloat16, layout_a=MatrixLayout.RowMajor,
                                             dtype_b=torch.bfloat16, layout_b=MatrixLayout.ColumnMajor,
-                                            dtype_c=torch.float32, layout_c=MatrixLayout.RowMajor,
-                                            dtype_d=torch.float32, 
+                                            dtype_c=torch.bfloat16, layout_c=MatrixLayout.RowMajor,
+                                            dtype_d=torch.bfloat16, 
                                             dtype_accumulator=torch.float32))
 
 for tile_config in tile_configurations:
